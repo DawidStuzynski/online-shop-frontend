bbc02c86a7640522f2bc32f9b23d47f152eea16d
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-11-18 09:34:23 +0100

    feat: Add fetching produtcs from backend

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,38 +1,45 @@
 import {Injectable} from '@angular/core';
 import {Product} from "./model/product";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
-  getProducts(): Product[] {
-    return [
-      {
-        name: "Product 1",
-        category: "Category 1",
-        description: "Product description",
-        price: 11.99,
-        currency: "PLN"
-      },
-      {
-        name: "Product 1",
-        category: "Category 1",
-        description: "Product description",
-        price: 11.99,
-        currency: "PLN"
-      },
-      {
-        name: "Product 1",
-        category: "Category 1",
-        description: "Product description",
-        price: 11.99,
-        currency: "PLN"
-      }
-    ]
+  getProducts(): Observable<Product[]> {
+
+    return this.http.get<Product[]>("http://localhost:8080/products")
+
+    //todo 42min
+
+    // return [
+    //   {
+    //     name: "Product 1",
+    //     category: "Category 1",
+    //     description: "Product description",
+    //     price: 11.99,
+    //     currency: "PLN"
+    //   },
+    //   {
+    //     name: "Product 1",
+    //     category: "Category 1",
+    //     description: "Product description",
+    //     price: 11.99,
+    //     currency: "PLN"
+    //   },
+    //   {
+    //     name: "Product 1",
+    //     category: "Category 1",
+    //     description: "Product description",
+    //     price: 11.99,
+    //     currency: "PLN"
+    //   }
+    // ]
 
   }
 }
