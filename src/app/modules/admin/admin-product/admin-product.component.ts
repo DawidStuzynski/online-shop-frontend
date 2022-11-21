8ad3dc5c1cdaafde701f19246431c0780973275d
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-11-21 16:09:02 +0100

    feat: Add pagination to admin product

--- src/app/modules/admin/admin-product/admin-product.component.ts
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -1,20 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {AdminProduct} from "./adminProduct";
+import {AdminProductService} from "./admin-product.service";
+import {MatPaginator} from "@angular/material/paginator";
+import {map, startWith, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-admin-product',
   templateUrl: './admin-product.component.html',
   styleUrls: ['./admin-product.component.scss']
 })
-export class AdminProductComponent implements OnInit {
+export class AdminProductComponent implements AfterViewInit {
 
-  dataSource: AdminProduct[] = [];
+  // dataSource: AdminProduct[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   displayedColumns: string[] = ["id", "name", "price"];
+  totalElements: number = 0;
+  data: AdminProduct[] = [];
 
-  constructor() {
+  constructor(private adminProductService: AdminProductService) {
   }
 
-  ngOnInit(): void {
-  }
 
+  ngAfterViewInit(): void {
+    this.paginator.page.pipe(
+      startWith({}),
+      switchMap(() => {
+        return this.adminProductService.getProducts(this.paginator.pageIndex, this.paginator.pageSize);
+      }),
+    ).subscribe(data => {
+      this.totalElements = data.totalElements;
+      this.data = data.content
+    })
+  }
 }
