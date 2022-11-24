9cd7bce51c4751b87b62862bfef894abeebcaa59
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-11-24 21:13:36 +0100

    feat: Implement edit product

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -8,6 +8,8 @@ import {AdminProductComponent} from "../../modules/admin/admin-product/admin-pro
 import {MaterialModule} from "../../shared/material.module";
 import {AdminProductUpdateComponent} from "../../modules/admin/admin-product-update/admin-product-update.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {AdminProductAddComponent} from "../../modules/admin/admin-product-add/admin-product-add.component";
+import {AdminProductFormComponent} from "../../modules/admin/admin-product-form/admin-product-form.component";
 
 
 @NgModule({
@@ -15,7 +17,10 @@ import {ReactiveFormsModule} from "@angular/forms";
     FullpageadminComponent,
     AdminComponent,
     AdminProductComponent,
-    AdminProductUpdateComponent
+    AdminProductUpdateComponent,
+    AdminProductAddComponent,
+    AdminProductFormComponent
+
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/admin/admin-product-form/admin-product-form.component.ts
@@ -0,0 +1,41 @@
+import {Component, Input, OnInit} from "@angular/core";
+import {FormGroup} from "@angular/forms";
+
+
+@Component({
+  selector: 'app-admin-product-form',
+  template: `
+    <div [formGroup]="parentForm" fxLayout="column">
+      <mat-form-field appearance="fill">
+        <mat-label>Name</mat-label>
+        <input matInput placeholder="Enter product name" formControlName="name">
+      </mat-form-field>
+
+      <mat-form-field appearance="fill">
+        <mat-label>Description</mat-label>
+        <textarea matInput rows="20" placeholder="Enter product description" formControlName="description"></textarea>
+      </mat-form-field>
+
+      <mat-form-field appearance="fill">
+        <mat-label>Price</mat-label>
+        <input matInput placeholder="Enter product price" formControlName="price">
+      </mat-form-field>
+
+      <mat-form-field appearance="fill">
+        <mat-label>Currency</mat-label>
+        <input matInput placeholder="Enter product currency" formControlName="currency">
+      </mat-form-field>
+
+      <div fxFlexAlign="end">
+        <button mat-flat-button color="primary">Save</button>
+      </div>
+    </div>
+  `
+})
+export class AdminProductFormComponent implements OnInit {
+  @Input() parentForm!: FormGroup;
+
+  ngOnInit(): void {
+
+  }
+}
