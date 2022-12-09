926c1138f44baf86b5a918e6a702e41db46eef4d
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2022-12-09 12:55:45 +0100

    feat: Implement adding image to product

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -1,38 +1,38 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FullpageadminComponent} from "./fullpageadmin.component";
-import {AdminComponent} from "../../modules/admin/admin.component";
-import {RouterModule} from "@angular/router";
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {AdminProductComponent} from "../../modules/admin/admin-product/admin-product.component";
-import {MaterialModule} from "../../shared/material.module";
-import {AdminProductUpdateComponent} from "../../modules/admin/admin-product-update/admin-product-update.component";
-import {ReactiveFormsModule} from "@angular/forms";
-import {AdminProductAddComponent} from "../../modules/admin/admin-product-add/admin-product-add.component";
-import {AdminProductFormComponent} from "../../modules/admin/admin-product-form/admin-product-form.component";
-import {AdminMessageComponent} from "../../modules/admin/admin-message/admin-message.component";
-import {AdminConfirmDialogComponent} from "../../modules/admin/admin-confirm-dialog/admin-confirm-dialog.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullpageadminComponent } from "./fullpageadmin.component";
+import { AdminComponent } from "../../modules/admin/admin.component";
+import { RouterModule } from "@angular/router";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AdminProductComponent } from "../../modules/admin/admin-product/admin-product.component";
+import { MaterialModule } from "../../shared/material.module";
+import { AdminProductUpdateComponent } from "../../modules/admin/admin-product-update/admin-product-update.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AdminProductAddComponent } from "../../modules/admin/admin-product-add/admin-product-add.component";
+import { AdminProductFormComponent } from "../../modules/admin/admin-product-form/admin-product-form.component";
+import { AdminMessageComponent } from "../../modules/admin/admin-message/admin-message.component";
+import { AdminConfirmDialogComponent } from "../../modules/admin/admin-confirm-dialog/admin-confirm-dialog.component";
 
 
 @NgModule({
-  declarations: [
-    FullpageadminComponent,
-    AdminComponent,
-    AdminProductComponent,
-    AdminProductUpdateComponent,
-    AdminProductAddComponent,
-    AdminProductFormComponent,
-    AdminMessageComponent,
-    AdminConfirmDialogComponent
+    declarations: [
+        FullpageadminComponent,
+        AdminComponent,
+        AdminProductComponent,
+        AdminProductUpdateComponent,
+        AdminProductAddComponent,
+        AdminProductFormComponent,
+        AdminMessageComponent,
+        AdminConfirmDialogComponent
 
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FlexLayoutModule,
-    MaterialModule,
-    ReactiveFormsModule
-  ]
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        FlexLayoutModule,
+        MaterialModule,
+        ReactiveFormsModule
+    ]
 })
 export class FullpageadminModule {
 }
--- src/app/modules/admin/admin-product-update/admin-product-update.component.ts
+++ src/app/modules/admin/admin-product-update/admin-product-update.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {AdminProductUpdate} from "./model/adminProductUpdate";
-import {AdminProductUpdateService} from "./admin-product-update.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {AdminMessageService} from "../admin-message.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { AdminProductUpdate } from "./model/adminProductUpdate";
+import { AdminProductUpdateService } from "./admin-product-update.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { AdminMessageService } from "../admin-message.service";
 
 @Component({
   selector: 'app-admin-product-update',
@@ -15,13 +15,16 @@ export class AdminProductUpdateComponent implements OnInit {
 
   product!: AdminProductUpdate;
   productForm!: FormGroup;
+  requiredFileTypes = "image/jpeg, image/png"
+  imageForm!: FormGroup;
+  image: string | null = null;
 
   constructor(
-    private router: ActivatedRoute,
-    private adminProductUpdateService: AdminProductUpdateService,
-    private formBuilder: FormBuilder,
-    private snackBar: MatSnackBar,
-    private adminMessageService: AdminMessageService
+      private router: ActivatedRoute,
+      private adminProductUpdateService: AdminProductUpdateService,
+      private formBuilder: FormBuilder,
+      private snackBar: MatSnackBar,
+      private adminMessageService: AdminMessageService
   ) {
   }
 
@@ -35,12 +38,16 @@ export class AdminProductUpdateComponent implements OnInit {
       price: ['', [Validators.required, Validators.min(0)]],
       currency: ['PLN', Validators.required]
     });
+
+    this.imageForm = this.formBuilder.group({
+      file: ['']
+    })
   }
 
   getProduct() {
     let id = Number(this.router.snapshot.params['id']);
     this.adminProductUpdateService.getProduct(id)
-      .subscribe(product => this.mapFormValues(product));
+        .subscribe(product => this.mapFormValues(product));
   }
 
   submit() {
@@ -51,22 +58,39 @@ export class AdminProductUpdateComponent implements OnInit {
       category: this.productForm.get('category')?.value,
       price: this.productForm.get('price')?.value,
       currency: this.productForm.get('currency')?.value,
+      image: this.image
     } as AdminProductUpdate).subscribe({
       next: product => {
         this.mapFormValues(product)
-        this.snackBar.open("Products saved successfully", '', {duration: 3000});
+        this.snackBar.open("Products saved successfully", '', { duration: 3000 });
       },
       error: err => this.adminMessageService.addSpringErrors(err.error)
     });
   }
 
-  private mapFormValues(product: AdminProductUpdate) {
+  private mapFormValues(product: AdminProductUpdate): void {
     this.productForm.setValue({
       name: product.name,
       description: product.description,
       category: product.category,
       price: product.price,
-      currency: product.currency
+      currency: product.currency,
     });
+    this.image = product.image
+  }
+
+  uploadFile() {
+    let formData = new FormData();
+    formData.append('file', this.imageForm.get('file')?.value);
+    this.adminProductUpdateService.uploadImage(formData)
+        .subscribe(result => this.image = result.filename)
+  }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.imageForm.patchValue({
+        file: event.target.files[0]
+      })
+    }
   }
 }
